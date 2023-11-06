239e8ac6833ec7bb37b847b070cb3d8645644c52
Thao.Tran <thaotran.k39ueh.qb@gmail.com>
2023-11-06 22:57:38 +0700

    coding endpoint filteredimage

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
+const {resolve} = require('path');
 
 (async () => {
 
@@ -14,29 +15,26 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   app.use(bodyParser.json());
 
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  app.get( "/filteredimage", async ( req, res) => {
+    const image_url: string = req.query.image_url as string;
+    let list_image_url : Array<string> = [];
+    // validate the image_url query
+    if(!image_url){
+      return res.status(400).send(`image_url is required`);
+    }
+    // Call filterImageFromURL(image_url) to filter the image
+    const resultPath = await filterImageFromURL(image_url);
+    if(!!resultPath){
+      list_image_url.push(resultPath);
+      res.status(200).sendFile(resultPath, async () => await deleteLocalFiles(list_image_url));
+    }
+    
+  });
 
-  /**************************************************************************** */
-
-  //! END @TODO1
-  
   // Root Endpoint
-  // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
-    res.send("try GET /filteredimage?image_url={{}}")
+    res.send("Hello I'm Alita ThaoTT26 From FPT")
   } );
-  
 
   // Start the Server
   app.listen( port, () => {
